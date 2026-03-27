4b92e0c5fadea82f4fc1e45c25e8cf97ac3163f7
Julio Jaramillo <juliocesar0293@gmail.com>
2026-03-27 16:21:05 -0600

    Testing express with JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola desde node js")
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 4000;
+//Routing
+
+app.get("/", (req, res) => {
+    res.send("Hello world from express");
+})
+
+app.get("/ecommerce", (req, res) => {
+    res.send("Hello world from ecommerce");
+})
+
+
+app.listen(PORT, () => {
+    console.info("Server working at port", PORT)
+})
